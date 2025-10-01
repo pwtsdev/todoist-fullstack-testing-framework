958f48a081795114e14204d4e2f230b7ecc8a744
bkita <2436425+bkita@users.noreply.github.com>
2025-10-01 10:21:09 +0200

    M04L05 - feat(tests): add API check for project creation test
    Include API request to verify project creation in the smoke test.

--- tests/smoke/create-project.spec.ts
+++ tests/smoke/create-project.spec.ts
@@ -1,4 +1,7 @@
 import { faker } from '@faker-js/faker';
+import { API_URL, PROJECTS_ENDPOINT } from '../../src/api/config/api.config';
+import { getRequest } from '../../src/api/requests/get.request';
+import { TODOIST_AUTH_HEADERS } from '../../src/api/requests/todoist.auth.headers';
 import { expect, test } from '../../src/fixtures/po.fixture';
 import { CreateProjectModel } from '../../src/models/create-project.model';
 
@@ -12,6 +15,9 @@ test('should create a new project', { tag: ['@smoke', '@smoke001'] }, async ({ h
   // Act
   await homePage.open();
   await homePage.leftPanel.addNewProject(project);
+
+  // API check
+  await getRequest(`${API_URL}/${PROJECTS_ENDPOINT}`, TODOIST_AUTH_HEADERS);
 
   // Assert
   await expect(homePage.leftPanel.getProjectByName(project.name)).toBeVisible();
